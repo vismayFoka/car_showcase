18a531c570248196a6703e92508af4d29bdc90e2
vismayFoka <fokavismay@gmail.com>
2024-10-06 11:32:59 +0530

    first

---
+++ components/CustomButton.tsx
@@ -0,0 +1,22 @@
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import CustomFilter from "./CustomFilter";
+import Searchbar from "./Searchbar";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    Searchbar,
+    CustomFilter,
+    SearchManufacturer,
+};
